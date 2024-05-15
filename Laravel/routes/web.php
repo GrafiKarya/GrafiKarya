1c79d2d9319b0c38bdf54f3b5cc1073d69c536f5
zufarzz <mochammadzufarz@gmail.com>
2024-05-15 07:18:32 +0700

    backend v2

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
-use App\Http\Controllers\GrafikKarya;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return view('daftarKarya');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/', [GrafikKarya::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
